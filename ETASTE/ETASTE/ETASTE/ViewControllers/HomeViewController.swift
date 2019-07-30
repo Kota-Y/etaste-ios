47e009a4685318c424d2799c8226409206dac845
hakuchan <kumamoncatch@icloud.com>
2019-07-31 00:29:53 +0900

    embed GoogleMap

---
+++ ETASTE/ETASTE/ETASTE/ViewControllers/HomeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  HomeViewContrie¥.swift
+//  ETASTE
+//
+//  Created by hakuchan on 2019/07/29.
+//  Copyright © 2019 ETASTE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import GoogleMaps
+
+
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var googleMap: GMSMapView!
+    
+    var shopModel: ShopModel = ShopModel()
+    
+    
+    
+    
+    override func viewDidLoad() {
+       
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let zoom: Float = 15
+        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: shopModel.latitude,longitude: shopModel.longitude, zoom: zoom)
+        googleMap.camera = camera
+        
+        let marker: GMSMarker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(shopModel.latitude, shopModel.longitude)
+        marker.map = googleMap
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
