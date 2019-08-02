ae888f0231ed6dfc71527166b1b58e415c0aef74
hakuchan <kumamoncatch@icloud.com>
2019-08-02 17:58:13 +0900

    Pinned into Map

--- ETASTE/ETASTE/ETASTE/ViewControllers/HomeViewController.swift
+++ ETASTE/ETASTE/ETASTE/ViewControllers/HomeViewController.swift
@@ -16,23 +16,35 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var googleMap: GMSMapView!
     
-    var shopModel: ShopModel = ShopModel()
-    
-    
+    var shopModel:[ShopModel] = []
+    var marker: [GMSMarker] = []
+    var lalo:[[Double]] = [[32.815183,130.727428],[32.814949,130.727842],[32.814419,130.726572]]
     
     
     override func viewDidLoad() {
        
+        
+        
     }
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        let zoom: Float = 15
-        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: shopModel.latitude,longitude: shopModel.longitude, zoom: zoom)
-        googleMap.camera = camera
         
-        let marker: GMSMarker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(shopModel.latitude, shopModel.longitude)
-        marker.map = googleMap
+        super.viewWillAppear(animated)
+        //shopmodel
+        for i in 0..<lalo.count{
+            let appendModel = ShopModel(latitude: lalo[i][0],longitude: lalo[i][1])
+            shopModel.append(appendModel)
+        }
+        //googlemap表示
+        let zoom: Float = 15
+        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: shopModel[0].latitude!,longitude: shopModel[0].longitude!, zoom: zoom)
+        googleMap.camera = camera
+        //マーカー表示
+        for i in 0..<lalo.count{
+            let mark: GMSMarker = GMSMarker()
+            mark.position = CLLocationCoordinate2DMake(shopModel[i].latitude!, shopModel[i].longitude!)
+            mark.map=googleMap
+            marker.append(mark)
+        }
 
     }
     
@@ -42,4 +54,7 @@ class HomeViewController: UIViewController {
     }
     
     
+    
+   
 }
+
