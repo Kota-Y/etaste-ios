9c1de31bac6e8ece81a710e01ff3d356a7fe6099
hakuchan <kumamoncatch@icloud.com>
2019-09-01 15:21:40 +0900

    setting2

--- ETASTE/ETASTE/ETASTE/ViewControllers/SettingViewController.swift
+++ ETASTE/ETASTE/ETASTE/ViewControllers/SettingViewController.swift
@@ -9,10 +9,35 @@
 import Foundation
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController  ,UITableViewDataSource, UITableViewDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        SettingTable.isScrollEnabled = false
+        SettingTable.tableFooterView = UIView()
         self.navigationItem.title = "設定"
     }
+    @IBOutlet weak var SettingTable: UITableView!
+    
+    let settingContent = ["プロフィール編集","Q＆A","お問い合わせ","オープンソース","利用規約","特定商品取引法","プライバシーポリシー","バージョン情報","ログアウト","退会について"]
+    
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = SettingTable.dequeueReusableCell(withIdentifier: "sCell", for: indexPath)
+        let label = cell.viewWithTag(1) as! UILabel
+        label.text =  String(settingContent[indexPath.row])
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        return cell
+    }
+
+    
+    
+    
 }
