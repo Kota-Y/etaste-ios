2216714849929a577d6f2a6ab11d5dbfc8e0e5d6
hakuchan <kumamoncatch@icloud.com>
2019-09-02 17:44:33 +0900

    rirefactoring1

--- ETASTE/ETASTE/ETASTE/ViewControllers/SettingViewController.swift
+++ ETASTE/ETASTE/ETASTE/ViewControllers/SettingViewController.swift
@@ -17,27 +17,72 @@ class SettingViewController: UIViewController  ,UITableViewDataSource, UITableVi
         self.navigationItem.title = "設定"
     }
     @IBOutlet weak var SettingTable: UITableView!
-    
-    let settingContent = ["プロフィール編集","Q＆A","お問い合わせ","オープンソース","利用規約","特定商品取引法","プライバシーポリシー","バージョン情報","ログアウト","退会について"]
-    
-    
-    
+}
+
+extension SettingViewController {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return SettingContent.allCases.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = SettingTable.dequeueReusableCell(withIdentifier: "sCell", for: indexPath)
-        let label = cell.viewWithTag(1) as! UILabel
-        label.text =  String(settingContent[indexPath.row])
+        let setting = SettingContent(rawValue: indexPath.row)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "sCell", for: indexPath)
+        cell.textLabel?.text = String(setting!.info.title)
         cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
         return cell
     }
+}
 
+enum SettingContent:Int, CaseIterable {
+    case profile
+    case qanda
+    case contact
+    case opensource
+    case termofservice
+    case speciffictransactions
+    case privacypolify
+    case versioninfo
+    case logout
+    case unsubscription
     
+    struct SettingData {
+        var title: String
+    }
     
-    
+    var info: SettingData {
+        switch self {
+        case .profile:
+            let settingData = SettingData(title: "プロフィール編集")
+            return settingData
+        case .qanda:
+            let settingData = SettingData(title: "Q & A")
+            return settingData
+        case .contact:
+            let settingData = SettingData(title: "お問い合わせ")
+            return settingData
+        case .opensource:
+            let settingData = SettingData(title: "オープンソース")
+            return settingData
+        case .termofservice:
+            let settingData = SettingData(title: "利用規約")
+            return settingData
+        case .speciffictransactions:
+            let settingData = SettingData(title: "特定商品取引法")
+            return settingData
+        case .privacypolify:
+            let settingData = SettingData(title: "プライバシーポリシー")
+            return settingData
+        case .versioninfo:
+            let settingData = SettingData(title: "バージョン情報")
+            return settingData
+        case .logout:
+            let settingData = SettingData(title: "ログアウト")
+            return settingData
+        case .unsubscription:
+            let settingData = SettingData(title: "退会について")
+            return settingData
+        }
+    }
 }
