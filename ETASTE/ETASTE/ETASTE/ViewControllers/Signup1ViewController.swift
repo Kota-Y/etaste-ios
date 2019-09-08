7ac967d89dca94928070306a38ab0daf0ee30f80
hakuchan <kumamoncatch@icloud.com>
2019-09-08 18:59:47 +0900

    keybord scroll

--- ETASTE/ETASTE/ETASTE/ViewControllers/Signup1ViewController.swift
+++ ETASTE/ETASTE/ETASTE/ViewControllers/Signup1ViewController.swift
@@ -21,6 +21,7 @@ class Signup1ViewController: UIViewController,UITextFieldDelegate {
         lastnamekatakana.delegate = self
         firstnamekatakana.delegate = self
         // Do any additional setup after loading the view.
+         configureObserver()
        
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -36,8 +37,12 @@ class Signup1ViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var errorlabel: UILabel!
     
     var flag1 = false
+    var t = false
     
     
+    @IBAction func wantscroll(_ sender: Any) {
+        t = true
+    }
     @IBAction func nextbutton(_ sender: Any) {
         let text1:String? = lastnamekanzi.text
         let text2:String? = firstnamekanzi.text
@@ -77,6 +82,42 @@ class Signup1ViewController: UIViewController,UITextFieldDelegate {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func configureObserver() {
+        
+        let notification = NotificationCenter.default
+        notification.addObserver(self, selector: #selector(keyboardWillShow(_:)),
+                                 name: UIResponder.keyboardWillShowNotification, object: nil)
+        notification.addObserver(self, selector: #selector(keyboardWillHide(_:)),
+                                 name: UIResponder.keyboardWillHideNotification, object: nil)
+        print("Notificationを発行")
+        
+    }
+    
+    /// キーボードが表示時に画面をずらす。
+    @objc func keyboardWillShow(_ notification: Notification?) {
+        if(t == true){
+        guard let rect = (notification?.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue,
+            let duration = notification?.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        UIView.animate(withDuration: duration) {
+            let transform = CGAffineTransform(translationX: 0, y: -(rect.size.height))
+            self.view.transform = transform
+        }
+        print("keyboardWillShowを実行")
+        }
+    }
+    
+    /// キーボードが降りたら画面を戻す
+    @objc func keyboardWillHide(_ notification: Notification?) {
+        if(t == true){
+        guard let duration = notification?.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? TimeInterval else { return }
+        UIView.animate(withDuration: duration) {
+            self.view.transform = CGAffineTransform.identity
+        }
+        print("keyboardWillHideを実行")
+      t = false
+    }
+    }
 
 }
 
