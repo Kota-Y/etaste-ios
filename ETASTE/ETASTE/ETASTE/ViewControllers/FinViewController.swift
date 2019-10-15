0ac3f0f4942a47de0e43bd8366f11c3a66f214ac
Shuto Nakano <xabdjx@gmail.com>
2019-10-15 15:55:39 +0900

    update layout

--- ETASTE/ETASTE/ETASTE/ViewControllers/FinViewController.swift
+++ ETASTE/ETASTE/ETASTE/ViewControllers/FinViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class FinViewController: UIViewController{
     
+    
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var finishButton: UIButton!
+    @IBOutlet weak var toHomeButton: UIButton!
+    
     override func viewDidLoad() {
         self.navigationItem.title = "購入完了"
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "loginicon")?.withRenderingMode(.alwaysOriginal),
+        /* self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "loginicon")?.withRenderingMode(.alwaysOriginal),
                                                                  style: .plain,
                                                                  target: self,
-                                                                 action: #selector(login))
+                                                                 action: #selector(login)) */
     }
 
-    @objc func login() {
+    /* @objc func login() {
         
-    }
+    } */
     
 }
